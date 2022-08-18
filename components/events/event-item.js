b00600ebe90e075564ca6a336c48923c2ed84ac4
Diego Felipe <diegofelipe.14@gmail.com>
2022-08-18 11:01:00 -0400

    update

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import Button from "../UI/button";
 
 import styles from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem({ title, image, date, location, id }) {
   const readableDate = new Date(date).toLocaleDateString("pt-BR", {
@@ -20,14 +24,21 @@ export default function EventItem({ title, image, date, location, id }) {
         <div className={styles.summary}>
           <h2>{title}</h2>
           <div className={styles.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={styles.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={styles.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Events</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
