f620d1b227ad32f70d17da3b5c3808c2e5f152ff
Diego Felipe <diegofelipe.14@gmail.com>
2022-08-25 21:09:47 -0400

    update

--- nextjs-course-code-03-prj-routing/pages/index.js
+++ nextjs-course-code-03-prj-routing/pages/index.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
+
 import { getFeaturedEvents } from "../helpers/api-utils";
 import EventList from "../components/events/event-list";
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>Nextjs Events</title>
+        <meta name="description" content="Find a lot of events" />
+      </Head>
       <EventList items={props.featuredEvents} />
     </div>
   );
